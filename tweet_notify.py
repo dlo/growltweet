3e6b104e20154a377f23ee38ad3248e486cba48d
Dan Loewenherz <dloewenherz@gmail.com>
2009-11-19 09:39:06 -0800

    add lists

--- tweet_notify.py
+++ tweet_notify.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-from httplib import HTTPConnection
 from base64 import encodestring
-import boto
+from httplib import HTTPConnection
 from json import loads
-import Growl as growl
 import urllib
+
+from constants import *
+import Growl as growl
+import boto
+
+LISTS = ['dwlz/news', 'dwlz/tech-cos']
 
 notifier = growl.GrowlNotifier("Twitter Update", [""])
 notifier.register()
@@ -13,7 +17,7 @@ notifier.register()
 sdb = boto.connect_sdb(AWS_KEY, AWS_SECRET)
 domain = sdb.create_domain("twitter")
 
-b64string = encodestring("%s:%s" % (username, password))
+b64string = encodestring("%s:%s" % (TWITTER_USERNAME, TWITTER_PASSWORD))
 conn = HTTPConnection("twitter.com")
 conn.request("GET", "/statuses/friends_timeline.json", "", {"Authorization": \
     "Basic %s" % b64string})
@@ -32,3 +36,23 @@ for tweet in tweets:
     icon = growl.Image.imageWithData(image_data)
     notifier.notify(noteType = "", title = item['screen_name'], description = \
         item['text'], icon = icon)
+
+for list in LISTS:
+  username, list_name = list.split("/")
+  conn.request("GET", "/1/%s/lists/%s/statuses.json" % (username, list_name), "", {})
+  tweets = loads(conn.getresponse().read())
+  for tweet in tweets:
+    exists = domain.get_item(tweet['id'])
+    if not exists:
+      item = domain.new_item(tweet['id'])
+      item['user_id'] = tweet['user']['id']
+      item['source'] = tweet['source']
+      item['text'] = tweet['text']
+      item['created_at'] = tweet['created_at']
+      item['screen_name'] = tweet['user']['screen_name']
+      item.save()
+      image_data = urllib.urlopen(tweet['user']['profile_image_url']).read()
+      icon = growl.Image.imageWithData(image_data)
+      notifier.notify(noteType = "", title = item['screen_name'], description = \
+          item['text'], icon = icon)
+
